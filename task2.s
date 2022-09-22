9bc33037557e0fee82ec7e7101895809abe90780
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-22 11:49:58 -0400

    Update task2.s

--- task2.s
+++ task2.s
@@ -3,7 +3,7 @@
 arr:   .word 68, -22, -31, 75, -10, -61, 39, 92, 94, -55 // test array of 10 elements 
 
 _start:
-	LDR R0,=arr //pointer to array 
+	LDR R0,=arr //pointer to array a.k.a base address of array 
 	MOV R1,#10 //length of array (n)
 	MOV R2,#1 //index i=1
 
@@ -12,30 +12,25 @@ TOP:
 	BLT OUTERLOOP //if R2<R1 => i<n => i-n<0 
 	
 OUTERLOOP:
-	MUL R3, R2, #4 //R3 = R2 *4 = i*4 //offset for index i 
-	ADD R4, R0, R3 //R4 = R0 + R3 = arr + i*4, absolute address of array 
-	LDR R5, [R4] //R5 = value = arr[i]
-	LDR R6, R2 //R6=j=i
+	LDR R3,[R0,R2,LSL#2] //R3=value=arr[i]
+	LDR R4, [R2] //R4=j=i
 	
-	CMP R6,#0 //R6-0
-	BGT INNERLOOP1 //if R6-0>0 => R6>0 then branch 
-	STR R5, [R0,R6,LSL#2]//insert arr[j] = value 
+	CMP R4,#0 //R4-0
+	BGT INNERLOOP1 //if R4-0>0 => R4>0 =>j>0 then branch 
+	STR R3, [R0,R2,LSL#2]// arr[j] = value 
 	ADD R2,R2,#1//i = i+1
 	B OUTERLOOP
 
 INNERLOOP1:
-	MUL R7, R6, #4//R7 = R6*4 = j*4
-	ADD R8, R0, R7//R8 = R0 + R7 = arr+j*4
-	SUB R8,R8,#1//R8 = arr+j*4 -1 
-	LDR R9,[R8]//R9 = arr[j-1]
-	CMP R9,R5//R9-R5 = arr[j-1]-value
+	SUB R5,R5,#1 //R5 = j-1
+	LDR R6,[R0,R5,LSL#4] //R6=arr[j-1]
+	CMP R6,R3//R6-R3 = arr[j-1]-value
 	BGT INNERLOOP2//if arr[j-1]-value>0 => arr[j-1]>value then branch 
-	STR R5, [R0,R6,LSL#2]//insert arr[j] = value
+	STR R3, [R0,R2,LSL#2]// arr[j] = value
 	B OUTERLOOP
 	
 INNERLOOP2:
-	ADD R10,R8,#1//R10 = arr+j*4
-	LDR R11,[R10]//R11=arr[j]
-	STR R9,[R11]//arr[j]=arr[j-1]
-	SUB R6,R6,#1//R6=R6-1 => j=j-1
+	LDR R7,[R0,R4,LSL#4] //R7=arr[j]
+	STR R7,[R6]//arr[j]=arr[j-1]
+	SUB R4,R4,#1//R6=R6-1 => j=j-1
 	B INNERLOOP1  
