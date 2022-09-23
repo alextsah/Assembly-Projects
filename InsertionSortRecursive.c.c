bc0486ee7d1ae4b5bdb2b64e3779d50059dda538
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-23 10:51:23 -0400

    Add files via upload

---
+++ InsertionSortRecursive.c.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* Function to sort an array using insertion sort*/
+void insertionSort(int arr[], int n)
+{
+	int i, key, j;
+	for (i = 1; i < n; i++) {
+		key = arr[i];
+		j = i - 1;
+
+		while (j >= 0 && arr[j] > key) {
+			arr[j + 1] = arr[j];
+			j = j - 1;
+		}
+		arr[j + 1] = key;
+	}
+}
+
+int main()
+{
+	return 0;
+}
+
