7f24ece168bdf03c03aad9e9d13fdbfa946a8c23
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-22 22:39:44 -0400

    Update task2.s

--- task2.s
+++ task2.s
@@ -1,5 +1,4 @@
-.global _start
-//initialize array 
+/initialize array 
 arr:   .word 68, -22, -31, 75, -10, -61, 39, 92, 94, -55 // test array of 10 elements 
 
 _start:
@@ -9,15 +8,16 @@ _start:
 
 TOP:
 	CMP R2,R1 //R2-R1=i-n
-	BLE OUTERLOOP //if R2<R1 => i<n then branch 
+	BLT OUTERLOOP //if R2<R1 => i<n then branch 
 	B end
 	
 OUTERLOOP: 
 	LDR R3,[R0,R2,LSL#2] //R3=value=arr[i]
 	MOV R4, R2 //R4=j=i
+INNERLOOP2C:
 	CMP R4,#0 //R4-0
-	BGE INNERLOOP1 //if R4-0>0 => R4>0 =>j>0 then branch 
-	STR R3, [R0,R2,LSL#2]// arr[j] = value
+	BGT INNERLOOP1 //if R4-0>0 => R4>0 =>j>0 then branch 
+	STR R3, [R0,R4,LSL#2]// arr[j] = value
 	ADD R2,R2,#1//i = i+1
 	B TOP
 
@@ -25,16 +25,17 @@ INNERLOOP1:
 	SUB R5,R4,#1 //R5 = j-1
 	LDR R6,[R0,R5,LSL#2] //R6=arr[j-1]
 	CMP R6,R3//R6-R3 = arr[j-1]-value
-	BGE INNERLOOP2//if arr[j-1]-value>0 => arr[j-1]>value then branch 
-	STR R3, [R0,R2,LSL#2]// arr[j] = value
+	BGT INNERLOOP2//if arr[j-1]-value>0 => arr[j-1]>value then branch 
+	STR R3, [R0,R4,LSL#2]// arr[j] = value
 	ADD R2,R2,#1//i = i+1
 	B TOP
 	
 INNERLOOP2:
-	LDR R7,[R0,R5,LSL#2] //R7=arr[j-1]
-	STR R7,[R0,R4,LSL#2]//arr[j]=arr[j-1]
+	STR R6,[R0,R4,LSL#2]//arr[j]=arr[j-1]
+	MOV R7,R3
+	STR R7,[R0,R5,LSL#2]
 	SUB R4,R4,#1//R6=R6-1 => j=j-1
-	ADD R2,R2,#1//i = i+1
-	B TOP
+	//ADD R2,R2,#1//i = i+1
+	B INNERLOOP2C
 
 .end
