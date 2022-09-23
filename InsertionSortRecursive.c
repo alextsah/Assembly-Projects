894fb3f03f5de2d93145faa12bd96a0ba206f602
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-23 10:54:55 -0400

    Update InsertionSortRecursive.c

--- InsertionSortRecursive.c
+++ InsertionSortRecursive.c
@@ -2,16 +2,15 @@
 
 void insertionSort(int arr[], int n)
 {
-	int i, key, j;
-	for (i = 1; i < n; i++) {
-		key = arr[i];
-		j = i - 1;
+	for (int i = 1; i < n; i++) {
+		int value = arr[i];
+		int j = i - 1;
 
-		while (j >= 0 && arr[j] > key) {
+		while (j >= 0 && arr[j] > value) {
 			arr[j + 1] = arr[j];
 			j = j - 1;
 		}
-		arr[j + 1] = key;
+		arr[j + 1] = value;
 	}
 }
 
