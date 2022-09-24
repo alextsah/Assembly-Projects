9189434ecd939d4b58765516489e12ca49a2f783
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-24 13:48:17 -0400

    Update SumOfNumbers.s

--- SumOfNumbers.s
+++ SumOfNumbers.s
@@ -1,33 +1,31 @@
 .global _start
 
-arr: 	.word 1,2,3,4,5 //array to be summed 
-n:		.word 5 //n=num of elements in array 
+arr: 	.word 1,2,3,4,5,5,10 //array to be summed 
+n:		.word 7 //n=num of elements in array 
 sum: 	.space 4 //space to store result 
 
 _start:
-	LDR R1, =arr
-	LDR R2, n
-	PUSH {R1, R2, LR}
-	BL add
-	LDR R1, [SP, #0]
-	STR R1, sum
-	ADD SP, SP, #8
-	POP {LR}
+	LDR R1, =arr //R1 = pointer to array 
+	LDR R2, n //R2 = num of elements to add 
+	PUSH {R1, R2, LR} //push parameters and LR(R1=pointer to array is TOS)
+	BL add //call add function
+	LDR R1, [SP, #0] //sum is at TOS (line 27)
+	STR R1, sum //store sum in memory 
+	ADD SP, SP, #8 //clear parameters 
+	POP {LR} //restore LR 
 	B end
 add:
-	PUSH {R4-R6}
-	LDR R4, [SP, #16]
-	LDR R5, [SP, #12]
-	MOV R1, #0
+	PUSH {R4-R6} //save the registers (to stack) add functions  uses 
+	LDR R4, [SP, #16] //load n from stack 
+	LDR R5, [SP, #12] //load pointer to array(R1) from stack 
+	MOV R1, #0 //clear R1 (sum)
 loop:
-	LDR R6,[R5], #4
-	ADD R1,R1,R6
-	SUBS R4,R4,#1
+	LDR R6,[R5], #4 //get next value from array 
+	ADD R1,R1,R6 //sum partially 
+	SUBS R4,R4,#1 //decrement loop counter 
 	BGT loop
-	STR R1, [SP, #12]
-	POP {R4-R6}
+	STR R1, [SP, #12] //store sum on stack, replacing array pointer 
+	POP {R4-R6} //restore registers 
 	BX LR
 	
-.end
-	
-	
+.end	
