a289317c772b0282745f15072a63280441852307
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-26 15:19:01 -0400

    Update task3.s

--- task3.s
+++ task3.s
@@ -2,45 +2,40 @@
 
 arr: 	.word 69,-22,-30
 _start:
-	LDR R0,=arr //pointer to array 
-	MOV R1,#3 //lenght of array (n)
+	LDR R0,=arr //R0=pointer to array 
+	MOV R1,#4 //R1=lenght of array (n)
 	
 	CMP R1,#1
 	BLE end
-	//PUSH {R0,R1,LR} //push parameters and LR (R0 is TOS)
-	
+	 
 OUTERLOOP:
-	//PUSH {R1,R2,LR} //save registers insertionsort uses to stack 
-	//LDR R1, [SP, #16] //load n from stack 
-	//LDR R2, [SP, #12] //load array pointer from stack 
 	PUSH {LR}
+	SUB R1,R1,#1
 	BL INNERLOOP2C //call subroutine
 	
 INNERLOOP2C:
 	SUB R2, R1, #1 //R2 = n-1
-	LDR R4, [R0,R2,LSL#2] //R4=value=arr[n-1]
-	SUB R5, R1,#2 //R5=j=n-2
+	LDR R3, [R0,R2,LSL#2] //R3=value=arr[n-1]
+	SUB R4, R1,#2 //R4=j=n-2
 	
-	CMP R5,#0 //branch if j>=0
+	CMP R4,#0 //branch if j>=0
 	BGE INNERLOOP1
-	ADD R6,R6,#1//R6=j+1
-	LDR R7,[R0,R6,LSL#2]//R7=arr[j+1]
-	STR R4,[R0,R6,LSL#2]
+	ADD R5,R4,#1//R5=j+1
+	STR R3,[R0,R5,LSL#2]//value ->arr[j+1]
 	B OUTERLOOP
 	
 INNERLOOP1:
-	LDR R8, [R0,R5,LSL#2] //R8 = arr[j]
-	CMP R8,R4 //arr[j] -arr[n-1]
-	BGT INNERLOOP2 //branch if R8>R4 =>arr[j]>value
-	STR R4,[R0,R6,LSL#2]
+	LDR R6, [R0,R4,LSL#2] //R6 = arr[j]
+	CMP R6,R3 //arr[j] - value
+	BGT INNERLOOP2 //branch if R6>R3 =>arr[j]>value
+	STR R3,[R0,R5,LSL#2] //value ->arr[j+1]
 	B OUTERLOOP
 	
 INNERLOOP2:
-	MOV R9,R8 //temp var for R9=R8=arr[j]
-	LDR R7,[R0,R6,LSL#2]
-	STR R7,[R0,R5,LSL#2] //arr[j+1] -> arr[j]
-	STR R9,[R0,R6,LSL#2] //arr[j] -> arr[j+1]
-	SUB R5,R5,#1 //j=j-1
+	LDR R7,[R0,R5,LSL#2] //R7 = arr[j+1]
+	STR R7,[R0,R4,LSL#2] //arr[j+1] -> arr[j]
+	STR R6,[R0,R5,LSL#2] //arr[j] -> arr[j+1]
+	SUB R4,R4,#1 //j=j-1
 	BX LR
 	
 .end
