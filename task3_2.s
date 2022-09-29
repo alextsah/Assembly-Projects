7a111cc8d278407675f936d75bd2ce294f6f5b64
alextsah <98911345+alextsah@users.noreply.github.com>
2022-09-29 14:35:16 -0400

    Update task3_2.s

--- task3_2.s
+++ task3_2.s
@@ -1,38 +1,35 @@
 .global _start
 
-arr: 	.word 69,-22,35,12,40
-n: 		.word 5
+arr: 	.word 69,-22,35,12,40,10,-30,77
+n: 		.word 8
 _start:
 
 	LDR R0,=arr //R0=pointer to array 
 	LDR R1,n //R1=lenght of array (n)
-	LDR R8,n
+	
 TOP:
-	//ADD SP, SP, #8
 	CMP R1,#1
-	PUSH {LR}
 	BLE OUTERLOOP// branch if n<=1
 	SUB R1,R1,#1
-	POP {LR}
 	B TOP
 	 
 OUTERLOOP:
 	BL INNERLOOP2C //call subroutine
 	
 INNERLOOP2C:
+	PUSH {R4-R8,LR}
+	LDR R8,n
 	ADD R1,R1,#1
-	PUSH {R8}
 	CMP R1,R8
 	BGT end
 	SUB R2, R1, #1 //R2 = n-1
-	PUSH {R4-R7}
 	LDR R3, [R0,R2,LSL#2] //R3=value=arr[n-1]
 	SUB R4, R1,#2 //R4=j=n-2
 	ADD R5,R4,#1//R5=j+1
 	CMP R4,#0 //branch if j>=0
 	BGE INNERLOOP1
 	STR R3,[R0,R5,LSL#2]//value ->arr[j+1]
-	POP {R4-R8}
+	POP {R4-R8, LR}
 	BX LR
 	
 INNERLOOP1:
@@ -40,7 +37,7 @@ INNERLOOP1:
 	CMP R6,R3 //arr[j] - value
 	BGT INNERLOOP2 //branch if R6>R3 =>arr[j]>value
 	STR R3,[R0,R5,LSL#2] //value ->arr[j+1]
-	POP {R4-R8}
+	POP {R4-R8,LR}
 	BX LR
 	
 INNERLOOP2:
@@ -48,7 +45,8 @@ INNERLOOP2:
 	STR R7,[R0,R4,LSL#2] //arr[j+1] -> arr[j]
 	STR R6,[R0,R5,LSL#2] //arr[j] -> arr[j+1]
 	SUB R4,R4,#1 //j=j-1
-	POP {R4-R8}
-	B TOP
+	POP {R4-R8,LR}
+	BX LR
 	
-.end
+end:
+	B end
