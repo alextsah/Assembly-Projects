a1d23d9fbdb35f2ff23056a860b36773963f214f
alextsah <98911345+alextsah@users.noreply.github.com>
2022-10-05 10:40:34 -0400

    Update task1.s

--- task1.s
+++ task1.s
@@ -1,21 +1,21 @@
 .global _start              
 // initialize values
-devident: .word 12
-devisor: .word 4
+devident: .word 15
+devisor: .word 6
 result: .space 4
 quotient: .word 0
 
 _start:
 	LDR R0,devident
 	LDR R1,devisor
-	LDR R2,=result
+	//LDR R2,=result
 	LDR R3,devident //R3 = reminder = devident
 	LDR R4,quotient //R4 = quotient 	
 top:
 	CMP R3,R1
 	BGE while
-	ADD R5,R3,R4,LSL#16
-	MOV R2,R5 // R2 = reminder + quotient *4 
+	ADD R2,R3,R4,LSL#16
+	STR R2, result // R2 = reminder + quotient *4 
 	B end
 while:
 	SUB R3,R3,R1 //Reminder = Reminder - Devisor 
